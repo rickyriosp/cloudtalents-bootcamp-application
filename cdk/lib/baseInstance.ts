d61cdb891bf4b0d3e9bc3cfb47da4fbebc7be132
rickyriosp <riki.rios91@gmail.com>
2025-04-26 21:00:14 -0400

    create ami

--- cdk/lib/baseInstance.ts
+++ cdk/lib/baseInstance.ts
@@ -8,8 +8,6 @@ export interface BaseInstanceProps {}
 export class BaseInstance extends Construct {
   readonly instanceId: string;
 
-  readonly ec2SecurityGroup: ec2.ISecurityGroup;
-
   constructor(scope: Construct, id: string, props?: BaseInstanceProps) {
     super(scope, id);
 
@@ -25,20 +23,15 @@ export class BaseInstance extends Construct {
     // ----------------------------------------------------------------------
     // Security Group
     // ----------------------------------------------------------------------
-    try {
-      this.ec2SecurityGroup = ec2.SecurityGroup.fromLookupByName(
-        this,
-        'ec2-sg',
-        'Allow EC2 SSH Access',
-        vpc,
-      );
-    } catch (error) {
-      this.ec2SecurityGroup = new ec2.SecurityGroup(this, 'ec2-sg', {
-        vpc: vpc,
-        securityGroupName: 'Allow EC2 SSH Access',
-      });
-    }
-    this.ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.SSH, 'SSH Access');
+    const ec2SecurityGroup = new ec2.SecurityGroup(this, 'ec2-sg', {
+      vpc: vpc,
+      allowAllOutbound: true,
+      securityGroupName: 'ec2-ssh-access',
+      description: 'Allow EC2 SSH Access',
+    });
+
+    ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.SSH, 'SSH Access');
+    ec2SecurityGroup.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
     // ----------------------------------------------------------------------
     // EC2 Role
@@ -47,6 +40,8 @@ export class BaseInstance extends Construct {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')],
     });
+
+    ec2Role.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
     // ----------------------------------------------------------------------
     // EC2 Instance
@@ -58,7 +53,7 @@ export class BaseInstance extends Construct {
         subnets: vpc.publicSubnets,
         onePerAz: false,
       }),
-      securityGroup: this.ec2SecurityGroup,
+      securityGroup: ec2SecurityGroup,
       role: ec2Role,
       machineImage: ec2.MachineImage.latestAmazonLinux2(),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
@@ -75,9 +70,9 @@ export class BaseInstance extends Construct {
       //userData: ec2.UserData.forLinux(),
       //init: ec2.CloudFormationInit.fromElements(),
     });
-
-    baseInstance.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    
     // baseInstance.addUserData()
+    baseInstance.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
     this.instanceId = baseInstance.instanceId;
   }
