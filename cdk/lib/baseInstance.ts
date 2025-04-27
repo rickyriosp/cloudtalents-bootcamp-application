5896ef51893b6c35ed4843d47b6bf91a17eac1a2
rickyriosp <riki.rios91@gmail.com>
2025-04-27 14:55:07 -0400

    .

--- cdk/lib/baseInstance.ts
+++ cdk/lib/baseInstance.ts
@@ -10,6 +10,10 @@ export interface BaseInstanceProps {}
 export class BaseInstance extends Construct {
   readonly instanceId: string;
 
+  readonly ec2SecurityGroup: ec2.ISecurityGroup;
+
+  readonly ec2Role: iam.IRole;
+
   constructor(scope: Construct, id: string, props?: BaseInstanceProps) {
     super(scope, id);
 
@@ -25,25 +29,41 @@ export class BaseInstance extends Construct {
     // ----------------------------------------------------------------------
     // Security Group
     // ----------------------------------------------------------------------
-    const ec2SecurityGroup = new ec2.SecurityGroup(this, 'ec2-sg', {
-      vpc: vpc,
-      allowAllOutbound: true,
-      securityGroupName: 'ec2-ssh-access',
-      description: 'Allow EC2 SSH Access',
-    });
+    try {
+      this.ec2SecurityGroup = ec2.SecurityGroup.fromLookupByName(
+        this,
+        'ec2-sg',
+        'ec2-ssh-access',
+        vpc,
+      );
+    } catch (error) {
+      this.ec2SecurityGroup = new ec2.SecurityGroup(this, 'ec2-sg', {
+        vpc: vpc,
+        allowAllOutbound: true,
+        securityGroupName: 'ec2-ssh-access',
+        description: 'Allow EC2 SSH Access',
+      });
+    }
 
-    ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.SSH, 'SSH Access');
-    ec2SecurityGroup.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    this.ec2SecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.SSH, 'SSH Access');
+    this.ec2SecurityGroup.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
     // ----------------------------------------------------------------------
     // EC2 Role
     // ----------------------------------------------------------------------
-    const ec2Role = new iam.Role(this, 'ec2-role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')],
-    });
+    try {
+      this.ec2Role = iam.Role.fromRoleName(this, 'ec2-role', 'ec2-role');
+    } catch (error) {
+      this.ec2Role = new iam.Role(this, 'ec2-role', {
+        roleName: 'ec2-role',
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        ],
+      });
+    }
 
-    ec2Role.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    this.ec2Role.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
     // ----------------------------------------------------------------------
     // EC2 Instance
@@ -55,20 +75,20 @@ export class BaseInstance extends Construct {
         subnets: vpc.publicSubnets,
         onePerAz: false,
       }),
-      securityGroup: ec2SecurityGroup,
-      role: ec2Role,
+      securityGroup: this.ec2SecurityGroup,
+      role: this.ec2Role,
       machineImage: ec2.MachineImage.latestAmazonLinux2(),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       associatePublicIpAddress: true,
-        blockDevices: [
-          {
-            deviceName: '/dev/xvda',
-            volume: ec2.BlockDeviceVolume.ebs(10, {
-              volumeType: ec2.EbsDeviceVolumeType.GENERAL_PURPOSE_SSD_GP3,
-              deleteOnTermination: true,
-            }),
-          },
-        ],
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          volume: ec2.BlockDeviceVolume.ebs(10, {
+            volumeType: ec2.EbsDeviceVolumeType.GENERAL_PURPOSE_SSD_GP3,
+            deleteOnTermination: true,
+          }),
+        },
+      ],
       userDataCausesReplacement: true,
       //   userData: ec2.UserData.forLinux(),
       init: ec2.CloudFormationInit.fromElements(
